350995a3af94e6227015794f42bacdebb9fd723b
Claudron <claudiowudang@gmail.com>
2023-07-06 16:44:30 +0200

    pull userData into UserAccountPage

--- DjangoReact/src/hooks/useUserData.ts
+++ DjangoReact/src/hooks/useUserData.ts
@@ -1,20 +1,26 @@
+import { useQuery } from '@tanstack/react-query';
 import apiClient from '../services/api-client';
+import User from '../entities/user';
 
-const useUserData = () => {
-    const userData = async () => {
-        try {
-          const response = await apiClient.get('/auth/users/me', { withCredentials: true });
-    
-          if (response.status === 200) {
-            console.log(response.data)
-            return response.data.access;
-          }
-        } catch (error) {
-          console.error(error);
-        }
-      };
-    
-      return userData;
+const fetchUserData = async () => {
+  const response = await apiClient.get<User>('/auth/users/me', { withCredentials: true });
+
+  if (response.status === 200) {
+      console.log(response.data)
+      return response.data;
+  }
+
+  throw new Error('Error fetching user data');
 }
 
-export default useUserData
+const useUserData = () => {
+  return useQuery<User, Error>(['userData'], {
+      queryFn: fetchUserData,
+      onError: (error) => console.error(error),
+  });
+}
+
+export default useUserData;
+
+
+
--- DjangoReact/src/pages/UserAccount.tsx
+++ DjangoReact/src/pages/UserAccount.tsx
@@ -1,18 +1,28 @@
-import { useEffect } from "react";
 import useUserData from "../hooks/useUserData";
 
 const UserAccount = () => {
-  const userData = useUserData();
+  const { data, isLoading, isError, error } = useUserData();
 
-  useEffect(() => {
-    const fetchUserData = async () => {
-      await userData();
-    };
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
 
-    fetchUserData();
-  }, [userData]);
+  if (isError) {
+    return <div>Error: {error.message}</div>;
+  }
 
-  return <div>UserAccount</div>;
+  if (data) {
+    return (
+      <div>
+        <h2>User data:</h2>
+        <p>Email: {data.email}</p>
+        <p>First Name: {data.first_name}</p>
+        <p>Last Name: {data.last_name}</p>
+      </div>
+    );
+  }
+
+  return null;
 };
 
 export default UserAccount;
