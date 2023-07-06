1d0a4605f96ad10a8aea808e5423fe90a59cc480
Claudron <claudiowudang@gmail.com>
2023-07-06 14:55:52 +0200

    add logout to persistLogin after error

--- DjangoReact/src/hooks/usePersistLogin.ts
+++ DjangoReact/src/hooks/usePersistLogin.ts
@@ -1,9 +1,13 @@
 import { useEffect, useState } from 'react';
 import apiClient from '../services/api-client';
+import { useNavigate } from 'react-router-dom';
+import useLogout from './useLogout';
 
 const usePersistLogin = () => {
     const [isAuthenticated, setIsAuthenticated] = useState(false);
     const [isLoading, setIsLoading] = useState(true); 
+    const navigate = useNavigate();
+    const logout = useLogout();
 
     useEffect(() => {
         const checkAuthStatus = async () => {
@@ -17,6 +21,9 @@ const usePersistLogin = () => {
                 // console.error(error);
                 console.log('please log in');
                 setIsAuthenticated(false);
+                logout();
+                navigate('/login');
+                
             } finally {
                 setIsLoading(false); // Set loading to false when done
             }
