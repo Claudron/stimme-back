dcd17395fda342925924f2a0c9bf1a01ce360f7b
Claudron <claudiowudang@gmail.com>
2023-07-20 22:27:40 +0200

    user activation hook and component

--- DjangoReact/src/hooks/useActivateUser.ts
+++ DjangoReact/src/hooks/useActivateUser.ts
@@ -1,15 +1,31 @@
-import { useMutation } from "@tanstack/react-query";
-import apiClient from "../services/api-client";
+import { useMutation, UseMutationOptions } from '@tanstack/react-query';
+import apiClient from '../services/api-client';
+import axios from 'axios';
 
-interface Params {
+interface ActivationData {
   uid: string;
   token: string;
 }
 
-const useActivateUser = () => useMutation(async (params: Params) => {
-  const res = await apiClient.post('/users/activation/', params);
-  console.log({ res });
-  return res.data;
-});
+const activateUser = async (activationData: ActivationData) => {
+  try {
+    const response = await apiClient.post('/auth/users/activation/', activationData);
+    return response.data;
+  } catch (error) {
+    // Check if error is AxiosError and get the server response error
+    if (axios.isAxiosError(error) && error.response) {
+      throw new Error(error.response.data.detail);
+    }
+    // If error is not AxiosError, re-throw it
+    throw error;
+  }
+};
+
+const useActivateUser = (options?: UseMutationOptions<any, Error, ActivationData>) => {
+  return useMutation<any, Error, ActivationData>(activateUser, {
+    retry: 3,  
+    ...options, 
+  });
+};
 
 export default useActivateUser;
--- DjangoReact/src/pages/UserActivationPage.tsx
+++ DjangoReact/src/pages/UserActivationPage.tsx
@@ -1,7 +1,39 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import useActivateUser from "../hooks/useActivateUser"; // Update the import path to your hook
+
+interface RouteParams {
+  uid: string;
+  token: string;
+  [key: string]: string | undefined;
+}
 
 const UserActivationPage = () => {
-  return <div>UserActivationPage</div>;
+  const { uid, token } = useParams<RouteParams>();
+  const { mutate, ...mutation } = useActivateUser();
+
+  useEffect(() => {
+    if (uid && token) {
+      mutate({ uid, token });
+    }
+  }, [uid, token, mutate]);
+
+  // Handle loading, error, and success states
+  if (mutation.isLoading) {
+    return <div>Activating account...</div>;
+  } else if (mutation.isError) {
+    // Ensure error is an instance of Error before accessing message property
+    const errorMessage =
+      mutation.error instanceof Error
+        ? mutation.error.message
+        : "An error occurred";
+    return <div>{errorMessage}</div>;
+  } else if (mutation.isSuccess) {
+    return <div>Account activated successfully!</div>;
+  } else {
+    // Default case, when mutation has not started yet
+    return null;
+  }
 };
 
 export default UserActivationPage;
