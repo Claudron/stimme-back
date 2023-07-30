2a59f4ac2fdc1f87a74c9e6b4c9252efc41b59ad
Claudron <claudiowudang@gmail.com>
2023-07-30 19:58:21 +0200

    fix and simplify content serializer

--- content/serializers.py
+++ content/serializers.py
@@ -1,14 +1,9 @@
 from rest_framework import serializers
 from .models import Content
 
-class ContentSerializer(serializers.ModelSerializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
-    body = serializers.CharField()
-    date_created = serializers.DateTimeField()
-    date_update = serializers.DateTimeField()
-    featured_image = serializers.ImageField()
 
+class ContentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Content
-        fields = ['id', 'title', 'body', 'date_created', 'date_update', 'featured_image']
+        fields = ['id', 'title', 'body', 'date_created',
+                  'date_update', 'thumbnail', 'content_image']
