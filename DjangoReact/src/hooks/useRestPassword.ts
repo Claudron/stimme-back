92359b6a2ecdd7e0bbe4bab418957fe5414b79c2
Claudron <claudiowudang@gmail.com>
2023-07-30 10:46:35 +0200

    passwordResetPage: better error messanges

--- DjangoReact/src/hooks/useRestPassword.ts
+++ DjangoReact/src/hooks/useRestPassword.ts
@@ -1,32 +1,59 @@
 import { useMutation } from '@tanstack/react-query';
 import apiClient from '../services/api-client';
+import axios from 'axios'; // Make sure to import axios if you are using it in apiClient
 
-interface RestPasswordData {
-  uid:string;
-  token: string  
+interface ResetPasswordData {
+  uid: string;
+  token: string;
   new_password: string;
   re_new_password: string;
 }
 
-const resetPassword = async (resetPasswordData: RestPasswordData) => {
+const resetPassword = async (resetPasswordData: ResetPasswordData) => {
+  try {
+    // Make a POST request to reset the password
     const response = await apiClient.post('/auth/users/reset_password_confirm/', resetPasswordData);
-  
+
+    // If the response status is 204, password reset was successful
     if (response.status === 204) {
       return true;
     }
-  
-    if (response.data) {
-      throw new Error(response.data.detail);
-    }
-  
+
+    // Throw a generic error if response status is not 204
     throw new Error('Error changing password');
-  }
+  } catch (error) {
+    // Check if the error is an Axios error and has a response
+    if (axios.isAxiosError(error) && error.response) {
+      // Extract all error messages from the server response
+      const customErrors = error.response.data;
 
-  const useResetPassword = () => {
-    return useMutation<boolean, Error, RestPasswordData>({
-      mutationFn: resetPassword,
-      onError: (error) => console.error(error),
-    });
-  }
+      // Initialize an empty error message string
+      let errorMessage = '';
 
-  export default useResetPassword;
+      // Iterate through each key (field) in the error object
+      for (const key in customErrors) {
+        // If the key has a value (i.e., error messages), concatenate them
+        if (customErrors[key]) {
+          errorMessage += customErrors[key].join(' ') + ' '; // Join error messages with a space and add a space at the end
+        }
+      }
+
+      // Trim the extra space from the end of the error message string and throw a new Error
+      throw new Error(errorMessage.trim());
+    } else {
+      // Throw a generic error if it's not an Axios error or doesn't have the expected structure
+      throw new Error('Error changing password');
+    }
+  }
+}
+
+
+
+const useResetPassword = () => {
+  return useMutation<boolean, Error, ResetPasswordData>({
+    mutationFn: resetPassword,
+    onError: (error) => console.error(error),
+  });
+}
+
+export default useResetPassword;
--- DjangoReact/src/pages/ResetPasswordPage.tsx
+++ DjangoReact/src/pages/ResetPasswordPage.tsx
@@ -7,6 +7,8 @@ import {
   Input,
   Alert,
   AlertIcon,
+  Container,
+  Flex,
 } from "@chakra-ui/react";
 import useResetPassword from "../hooks/useRestPassword";
 import { useRef } from "react";
@@ -29,7 +31,12 @@ const ResetPasswordPage = () => {
       const reNewPassword = reNewPasswordRef.current.value;
 
       resetPasswordMutation.mutate(
-        { uid, token, new_password: newPassword, re_new_password: reNewPassword },
+        {
+          uid,
+          token,
+          new_password: newPassword,
+          re_new_password: reNewPassword,
+        },
         {
           onSuccess: () => {
             if (newPasswordRef.current && reNewPasswordRef.current) {
@@ -49,41 +56,55 @@ const ResetPasswordPage = () => {
   };
 
   return (
-    <Box
-      borderWidth="1px"
-      borderRadius="lg"
-      p={4}
-      boxShadow="lg"
-      marginBottom={5}
-    >
-      <Heading marginBottom={5}>Reset Password</Heading>
-      <form onSubmit={handleSubmit}>
-        <FormControl>
-          <FormLabel>
-            New Password:
-            <Input ref={newPasswordRef} type="password" name="newPassword" required />
-          </FormLabel>
-          <FormLabel>
-            Repeat New Password:
-            <Input ref={reNewPasswordRef} type="password" name="reNewPassword" required />
-          </FormLabel>
-          <Button type="submit">Reset Password</Button>
-        </FormControl>
-      </form>
-      {resetPasswordMutation.isLoading && <div>Changing password...</div>}
-      {resetPasswordMutation.isError && (
-        <Alert status="error">
-          <AlertIcon />
-          {resetPasswordMutation.error.message}
-        </Alert>
-      )}
-      {resetPasswordMutation.isSuccess && (
-        <Alert status="success">
-          <AlertIcon />
-          Password has been changed successfully
-        </Alert>
-      )}
-    </Box>
+    <Container height="80vh" justifyContent="center" alignItems="center">
+      <Flex>
+        <Box
+          borderWidth="1px"
+          borderRadius="lg"
+          p={4}
+          boxShadow="lg"
+          marginBottom={5}
+        >
+          <Heading marginBottom={5}>Reset Password</Heading>
+          <form onSubmit={handleSubmit}>
+            <FormControl>
+              <FormLabel>
+                New Password:
+                <Input
+                  ref={newPasswordRef}
+                  type="password"
+                  name="newPassword"
+                  required
+                />
+              </FormLabel>
+              <FormLabel>
+                Repeat New Password:
+                <Input
+                  ref={reNewPasswordRef}
+                  type="password"
+                  name="reNewPassword"
+                  required
+                />
+              </FormLabel>
+              <Button type="submit">Reset Password</Button>
+            </FormControl>
+          </form>
+          {resetPasswordMutation.isLoading && <div>Changing password...</div>}
+          {resetPasswordMutation.isError && (
+            <Alert status="error">
+              <AlertIcon />
+              {resetPasswordMutation.error.message}
+            </Alert>
+          )}
+          {resetPasswordMutation.isSuccess && (
+            <Alert status="success">
+              <AlertIcon />
+              Password has been changed successfully
+            </Alert>
+          )}
+        </Box>
+      </Flex>
+    </Container>
   );
 };
 
