d67abe71a017827274242fb487c851dc0f86a04f
Claudron <claudiowudang@gmail.com>
2023-08-04 12:08:39 +0200

    fix content serializer

--- content/serializers.py
+++ content/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
-from .models import Content
+from .models import Content, ContentImage
 
+class ContentImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ContentImage
+        fields = ['image']
 
 class ContentSerializer(serializers.ModelSerializer):
+    content_image = ContentImageSerializer(source='images', many=True, read_only=True)
+
     class Meta:
         model = Content
         fields = ['id', 'title', 'body', 'date_created',
