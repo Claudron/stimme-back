8c233addfad940b4e5f34a7b112e3e83a4fbed09
Claudron <claudiowudang@gmail.com>
2023-08-06 20:28:03 +0200

    categories: migration and model admin

--- content/admin.py
+++ content/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import ContentImage
+from .models import ContentImage, Category
 from . import models
 
 
@@ -7,11 +7,18 @@ class ContentImageInline(admin.TabularInline):
     model = ContentImage
     extra = 1
 
+
 class ContentAdmin(admin.ModelAdmin):
     inlines = [ContentImageInline]
-    list_display = ('id', 'title', 'date_created')
+    list_display = ('id', 'title', 'date_created', 'display_categories')
     ordering = ('title', 'id', 'date_created')
+    search_fields = ('title',)
+    list_filter = ('date_created',)
+
+    def display_categories(self, obj):
+        return ", ".join(category.name for category in obj.category.all())
+    display_categories.short_description = 'Categories'
 
 
-
-admin.site.register(models.Content, ContentAdmin)
+admin.site.register(models.Content, ContentAdmin,)
+admin.site.register(Category)
