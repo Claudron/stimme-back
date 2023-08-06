43a6d3bed04d9a5ba46e89be2df5f52f5f572deb
Claudron <claudiowudang@gmail.com>
2023-08-06 20:39:11 +0200

    Categories Admin.py: Child / Parent relationship

--- content/admin.py
+++ content/admin.py
@@ -20,5 +20,29 @@ class ContentAdmin(admin.ModelAdmin):
     display_categories.short_description = 'Categories'
 
 
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'display_parent')
+    search_fields = ('name',)
+    list_filter = ('parent',)
+    ordering = ('parent__name', 'name',)
+
+    def display_parent(self, obj):
+        return " -> ".join(self.get_parents(obj))
+
+    def get_parents(self, obj):
+        # Create a list to store the names
+        names = []
+
+        # While there is a parent, add the parent's name to the list and move up the tree
+        while obj is not None:
+            names.append(obj.name)
+            obj = obj.parent
+
+        # Reverse the list so that the root is at the beginning and return it
+        return names[::-1]
+
+    display_parent.short_description = 'Parent Categories'
+
+
 admin.site.register(models.Content, ContentAdmin,)
-admin.site.register(Category)
+admin.site.register(Category, CategoryAdmin)
