50004a6ba672a8b3730489e9da5a702d487e8797
Claudron <claudiowudang@gmail.com>
2023-08-23 13:32:01 +0200

    playlist: styling buttons

--- DjangoReact/src/components/Playlist.tsx
+++ DjangoReact/src/components/Playlist.tsx
@@ -6,6 +6,8 @@ import {
   List,
   ListItem,
   useTheme,
+  Flex, 
+  VStack,
 } from "@chakra-ui/react";
 import { File } from "../store/useExerciseStore";
 import React from "react";
@@ -17,66 +19,63 @@ const Playlist = () => {
     (state) => state.currentTrackIndex
   );
   const setCurrentTrackIndex = usePlaylistStore((s) => s.setCurrentTrackIndex); // <-- Extract this from the store
+  const moveItemUp = usePlaylistStore((s) => s.moveItemUp);
+  const moveItemDown = usePlaylistStore((s) => s.moveItemDown);
+  
+  
   const theme = useTheme(); // <-- get the theme
   const selectedItemColor = theme.colors.teal[100];
   const LstItemHover = theme.colors.gray[600];
+  const removeItemColor = theme.colors.red[900];
 
   console.log(playlist);
 
-  const moveItemUp = usePlaylistStore((s) => s.moveItemUp);
-  const moveItemDown = usePlaylistStore((s) => s.moveItemDown);
 
 
   return (
     <List>
       {playlist?.map((data: File, index: number) => (
-        <React.Fragment key={data.uniqueId}>
-          <ListItem
-            style={
-              index === currentPlayingIndex
-                ? {
-                    border: `1px solid ${selectedItemColor}`,
-                    borderRadius: "2px",
-                    padding: "4px",
-                  }
-                : {}
-            }
-            onClick={() => setCurrentTrackIndex(index)}
-            _hover={{ backgroundColor: LstItemHover }}
-          >
-            {" "}
-            <Heading as="h3" size="lg">
-              {data.ExerciseName}
-            </Heading>
-            <Heading as="h5" size="sm">
-              {data.methodName}
-            </Heading>
-            <Badge colorScheme="green" marginRight={1}>
-              RANGE:
-            </Badge>
-            {data.range}
-            <Badge colorScheme="green" marginLeft={3} marginRight={1}>
-              {" "}
-              DIRECTION:
-            </Badge>
-            {data.direction}
-            <Badge colorScheme="green" marginLeft={3} marginRight={1}>
-              TEMPO:
-            </Badge>
-            {data.tempo}
-            <Button size="sx" onClick={() => moveItemUp(index)}>Move Up</Button>
-            <Button size="sx" onClick={() => moveItemDown(index)}>Move Down</Button>
-            <Button
-              marginLeft={3}
-              onClick={() => removeFromPlaylist(data.uniqueId)}
-            >
-              Remove
-            </Button>
-          </ListItem>
-        </React.Fragment>
+        <ListItem
+          key={data.uniqueId}
+          style={
+            index === currentPlayingIndex
+              ? {
+                  border: `1px solid ${selectedItemColor}`,
+                  borderRadius: "2px",
+                  padding: "4px",
+                }
+              : {}
+          }
+          onClick={() => setCurrentTrackIndex(index)}
+          _hover={{ backgroundColor: LstItemHover }}
+        >
+          <Flex justifyContent="space-between" width="100%">
+            {/* Left side content */}
+            <VStack align="start" spacing={1}>
+              <Heading as="h3" size="lg">{data.ExerciseName}</Heading>
+              <Heading as="h5" size="sm">{data.methodName}</Heading>
+              <Flex>
+                <Badge colorScheme="green" marginRight={1}>RANGE:</Badge>{data.range}
+                <Badge colorScheme="green" marginLeft={3} marginRight={1}>DIRECTION:</Badge>{data.direction}
+                <Badge colorScheme="green" marginLeft={3} marginRight={1}>TEMPO:</Badge>{data.tempo}
+              </Flex>
+            </VStack>
+  
+            {/* Right side (buttons) */}
+            <VStack align="end" spacing={1}>
+              <Flex width="100%">
+                <Button size="sm" flex="1" onClick={() => moveItemUp(index)}>Up</Button>
+                <Button size="sm" flex="1" onClick={() => moveItemDown(index)}>Down</Button>
+              </Flex>
+              <Button bg={removeItemColor}  size="md" width="100%" onClick={() => removeFromPlaylist(data.uniqueId)}>Remove</Button>
+            </VStack>
+          </Flex>
+        </ListItem>
       ))}
     </List>
   );
+  
+  
 };
 
 export default Playlist;
