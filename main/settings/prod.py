a873dd63b0770c65a140a051dc6f33f6a5ea851f
Claudron <claudiowudang@gmail.com>
2023-08-27 14:11:31 +0200

    4th Shipment

--- main/settings/prod.py
+++ main/settings/prod.py
@@ -1,6 +1,8 @@
 import os
 from .common import *
 import dj_database_url
+from .common import TEMPLATES as COMMON_TEMPLATES
+from .common import STATICFILES_DIRS as COMMON_STATICFILES_DIRS
 
 
 DEBUG = False
@@ -21,6 +23,16 @@ DATABASES = {
         conn_max_age=600
     )
 }
+
+
+STATICFILES_DIRS = COMMON_STATICFILES_DIRS + [
+        os.path.join(BASE_DIR, 'DjangoReact', 'dist', 'assets'),
+]
+
+TEMPLATES = COMMON_TEMPLATES.copy()
+TEMPLATES[0]['DIRS'] = [os.path.join(BASE_DIR, 'DjangoReact', 'dist')]
+
+
 
 LOGGING = {
     'version': 1,
