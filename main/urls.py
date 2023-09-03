b7b21474c236e003dbe4851c0d8c61b94ea2d8b5
Claudron <claudiowudang@gmail.com>
2023-09-03 22:32:24 +0200

    Shipment 13

--- main/urls.py
+++ main/urls.py
@@ -18,8 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 from core.views import TokenCreateView, RefreshTokenView, LogoutView, AuthStatusView, test_email
 from django.conf import settings
-from django.views.generic import TemplateView
-from django.urls import re_path
 from django.conf.urls.static import static
 
 
@@ -40,6 +38,3 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-if getattr(settings, 'SERVE_REACT_FRONTEND', False):
-    urlpatterns.append(re_path(r'^.*', TemplateView.as_view(template_name='index.html')
-))
