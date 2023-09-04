355ccfbe34d97a6711005142bb28da597e1b2527
Claudron <claudiowudang@gmail.com>
2023-09-04 18:53:07 +0200

    Shipment 23: GCS

--- main/wsgi.py
+++ main/wsgi.py
@@ -7,12 +7,19 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
+import tempfile
+import json
 import os
 from django.core.wsgi import get_wsgi_application
 
-# Add the print statement here
-print(os.environ.get("GS_CREDENTIALS"))
+# Convert the key content into a temporary file and set GOOGLE_APPLICATION_CREDENTIALS
+key_dict = json.loads(os.environ.get("GS_CREDENTIALS"))
+with tempfile.NamedTemporaryFile(delete=False) as temp:
+    temp.write(json.dumps(key_dict).encode('utf-8'))
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = temp.name
 
+# Set default Django settings module
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.settings.dev')
 
+# Initialize the Django application
 application = get_wsgi_application()
