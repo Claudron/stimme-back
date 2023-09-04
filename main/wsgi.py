c17ed95d5c335b441e3ffec3e2111062545f4dba
Claudron <claudiowudang@gmail.com>
2023-09-04 18:39:29 +0200

    Shipment 22: GCS

--- main/wsgi.py
+++ main/wsgi.py
@@ -8,9 +8,10 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
+# Add the print statement here
+print(os.environ.get("GS_CREDENTIALS"))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.settings.dev')
 
