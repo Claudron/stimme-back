14bffb351d19649e357028734404bbbebda5129f
Claudron <claudiowudang@gmail.com>
2023-09-05 19:56:14 +0200

    Shipment 34: SMTP

--- send_test_email.py
+++ send_test_email.py
@@ -1,17 +1,18 @@
-from django.core.management.base import BaseCommand
+import os
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project_name.settings')
+django.setup()
+
 from django.core.mail import send_mail
 
-class Command(BaseCommand):
-    help = 'Sends a test email using django-sendgrid-v5'
+subject = 'Hello from Django'
+message = 'This is a test email sent from Django using django-sendgrid-v5.'
+from_email = 'manzarimakesmusic@gmail.com'
+recipient_list = ['claudiowudang@gmail.com']
 
-    def handle(self, *args, **kwargs):
-        subject = 'Hello from Django'
-        message = 'This is a test email sent from Django using django-sendgrid-v5.'
-        from_email = 'manzarimakesmusic@gmail.com'  # Replace with your 'from' email address
-        recipient_list = ['claudiowudang@gmail.com']  # Replace with the recipient's email address
-
-        try:
-            send_mail(subject, message, from_email, recipient_list)
-            self.stdout.write(self.style.SUCCESS('Test email sent successfully!'))
-        except Exception as e:
-            self.stderr.write(self.style.ERROR(f'Failed to send email. Error: {e}'))
+try:
+    send_mail(subject, message, from_email, recipient_list)
+    print('Test email sent successfully!')
+except Exception as e:
+    print(f'Failed to send email. Error: {e}')
