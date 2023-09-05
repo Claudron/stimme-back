4db1359e6f3761e082bd9c8842bb9c379c1bb644
Claudron <claudiowudang@gmail.com>
2023-09-05 15:29:05 +0200

    Shipment 29: SMTP

--- main/settings/prod.py
+++ main/settings/prod.py
@@ -88,7 +88,10 @@ SESSION_COOKIE_SAMESITE = 'Lax'
 
 #Email
 
-EMAIL_BACKEND = 'sendgrid_backend.SendgridBackend'
-SENDGRID_API_KEY = os.environ.get("SENDGRID_API_KEY")
+SENDGRID_EMAIL_HOST = "smtp.sendgrid.net"
+SENDGRID_EMAIL_PORT = 465
+SENDGRID_EMAIL_USERNAME = "apikey"
+SENDGRID_EMAIL_PASSWORD = os.environ.get("SENDGRID_API_KEY")
+
 
 
