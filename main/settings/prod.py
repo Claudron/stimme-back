0d5b13f6397a4d2f1d20b35675550a6a79124ab5
Claudron <claudiowudang@gmail.com>
2023-09-07 15:43:28 +0200

    Shipment 35: SMTP

--- main/settings/prod.py
+++ main/settings/prod.py
@@ -89,7 +89,7 @@ SESSION_COOKIE_SAMESITE = 'Lax'
 #Email
 
 EMAIL_BACKEND = 'sendgrid_backend.SendgridBackend'
-DEFAULT_FROM_EMAIL = 'manzarimakesmusic@gmail.com'
+DEFAULT_FROM_EMAIL = 'stimme@manzarimusic.com'
 SENDGRID_API_KEY = os.environ["SENDGRID_API_KEY"]
 
 
