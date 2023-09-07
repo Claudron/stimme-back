90d1bbb7a58d3283f84ef16676da3ee3b649af40
Claudron <claudiowudang@gmail.com>
2023-09-07 17:05:43 +0200

    Shipment 39: Djoser settings

--- main/settings/prod.py
+++ main/settings/prod.py
@@ -15,7 +15,7 @@ DEBUG = False
 
 SECRET_KEY = os.environ["SECRET_KEY"]
 
-ALLOWED_HOSTS = ["localhost"]
+ALLOWED_HOSTS = ["stimme.onrender.com", "www.stimme.onrender.com", "stimme-back.onrender.com", "www.stimme-back.onrender.com"]
 
 SERVE_REACT_FRONTEND = True
 
