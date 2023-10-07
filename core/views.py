249bb57f9ca1fd1debfd818266cd4edd6cecba56
Claudron <claudiowudang@gmail.com>
2023-10-07 09:12:42 +0200

    Shipment 52: Safari cookies

--- core/views.py
+++ core/views.py
@@ -39,9 +39,9 @@ class TokenCreateView(DjoserTokenCreateView):
 
         response = Response({"detail": "Success"})
         response.set_cookie("access_token", str(
-            token.access_token), samesite='None', secure=django_settings.COOKIE_SECURE_SETTING, httponly=True, domain='.onrender.com', path='/')
+            token.access_token), samesite='None', secure=django_settings.COOKIE_SECURE_SETTING, httponly=True)
         response.set_cookie("refresh_token", str(
-            token), samesite='None', secure=django_settings.COOKIE_SECURE_SETTING, httponly=True, domain='.onrender.com', path='/')
+            token), samesite='None', secure=django_settings.COOKIE_SECURE_SETTING, httponly=True)
 
         return response
 
@@ -57,9 +57,9 @@ class RefreshTokenView(SimpleJWTTokenRefreshView):
                 'refresh': str(token),
             })
             response.set_cookie("access_token", str(
-                token.access_token), samesite='None', secure=django_settings.COOKIE_SECURE_SETTING, httponly=True, domain='.onrender.com', path='/')
+                token.access_token), samesite='None', secure=django_settings.COOKIE_SECURE_SETTING, httponly=True)
             response.set_cookie("refresh_token", str(
-                token), samesite='None', secure=django_settings.COOKIE_SECURE_SETTING, httponly=True, domain='.onrender.com', path='/')
+                token), samesite='None', secure=django_settings.COOKIE_SECURE_SETTING, httponly=True)
             return response
 
         except (InvalidToken, TokenError) as e:
